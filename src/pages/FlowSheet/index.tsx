021f5b28291f163270e560ba6b4d2d92c78c2905
GSemir0418 <845217811@qq.com>
2022-11-08 13:18:11 +0800

    feat: 基于antv-S2完成产品工序明细表绘制

--- src/pages/FlowSheet/index.tsx
+++ src/pages/FlowSheet/index.tsx
@@ -1,18 +1,15 @@
 import { PivotSheet } from '@antv/s2';
 import { useEffect } from 'react';
+import fakeSheetData from './fakeSheetData.json';
 const FlowSheet = () => {
   useEffect(() => {
-    fetch('https://gw.alipayobjects.com/os/bmw-prod/2a5dbbc8-d0a7-4d02-b7c9-34f6ca63cff6.json')
-      .then((res) => res.json())
-      .then((dataCfg) => {
-        const container = document.getElementById('flow-sheet-container')!;
-        const s2Options = {
-          width: document.body.clientWidth,
-          height: document.body.clientHeight,
-        };
-        const s2 = new PivotSheet(container, dataCfg, s2Options);
-        s2.render();
-      });
+    const container = document.getElementById('flow-sheet-container')!;
+    const s2Options = {
+      width: document.body.clientWidth,
+      height: document.body.clientHeight,
+    };
+    const s2 = new PivotSheet(container, fakeSheetData, s2Options);
+    s2.render();
   }, []);
   return <div id="flow-sheet-container" />;
 };
